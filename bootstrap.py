c13aa11d9c847d33eb481bfc3f31df2bea5e65b8
cobertos <me+git@cobertos.com>
2021-12-17 01:10:09 -0500

    Added lerna

--- bootstrap.py
+++ bootstrap.py
@@ -130,6 +130,7 @@ source {cobertosRCPath}/cobertos.profile
   NpmInstallGlobalOp("serverless")()  # Tool
   NpmInstallGlobalOp("eslint_d")()    # Dependency for Sublime Linter
   NpmInstallGlobalOp("js-yaml")()     # Tool
+  NpmInstallGlobalOp("lerna")()     # Tool
   # TODO:
   # Ability to use global packages in require() with NODE_PATH
   # Should work with NVM https://stackoverflow.com/a/49293370/2759427
