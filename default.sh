252bb7dc03031e0c6ebb9a50bc389ee9f78fa694
cobertos <me+git@cobertos.com>
2026-04-01 15:52:48 -0400

    Misc cleanup

--- default.sh
+++ default.sh
@@ -42,9 +42,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
-# colored GCC warnings and errors
-#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
